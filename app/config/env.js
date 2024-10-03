67ce170a5660e22979e57510fb65b964cdeb8b94
rolandocuma <rcumas@miumg.edu.gt>
2024-10-02 18:40:08 -0600

    secondd

--- app/config/env.js
+++ app/config/env.js
@@ -1,15 +1,15 @@
 const env = {
-    database: 'umg234200',
-    username: 'umg234200_user',
-    password: 'z7rpwJ0bCddBOugHq66xP5XiENI0Tb0i',
-    host: 'dpg-cr60rvbtq21c73b7hfmg-a.oregon-postgres.render.com',
-    dialect: 'postgres',  
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
+  database: 'umg23420024',
+  username: 'umg23420024_user',
+  password: '9uk2kAMI7YfWyLAtl7jXCIUwSXgRAfuU',
+  host: 'dpg-crq8ft88fa8c738uhcrg-a.oregon-postgres.render.com',
+  dialect: 'postgres',  
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
 };
 
 module.exports = env;
